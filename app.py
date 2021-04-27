8245f8b4bcfa10341cfe0ceaff4c29fcf4360677
Advali Tech Pty Ltd <bimalkprabha24@gmail.com>
2021-04-27 18:14:33 +0800

    update_complete

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-def main():
-    with open("code.txt",mode='r',encoding='utf8') as file:
+import re
+from os import replace
 
-        # use enumerate to show that second line is read as a whole
-        lines = file.readlines();
-        for line in lines:
-            print(line)
+def main():
+    replace_words = {
+        'reg_val_32_inst;':'reg_ptr;',
+        'reg_val_32=0;': 'exp_reg_data;',
+        'uvm_pkg::uvm_reg_data_t_read_field_vals[$];':'uvm_pkg::uvm_reg_data_t read_reg_data[$]'
+
+                    }
+
+    with open("code.txt",'r') as rf:
+        with open("code_new.txt",'w',encoding='utf-8') as wf:
+            line = rf.read()
+            for word in line.split():
+                print(word)
+                try:
+                    line = line.replace(word, replace_words[word])
+                except:
+                    pass
+            wf.write(line)
+
 
 
 if __name__ == "__main__":
